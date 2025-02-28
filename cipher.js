3fd202d0fc2b8a7798273335b39ea1ed42cc3adf
Enes Ercan Enwar Hurmuzlu <enesalhurmuzi@gmail.com>
2025-02-28 14:20:24 +0300

    Update cipher.js

--- cipher.js
+++ cipher.js
@@ -1,1 +1,11 @@
+// Base64 Decoder
+function decodeBase64(str) {
+  return atob(str);
+}
 
+// Caesar Cipher (Shift +7)
+function caesarDecrypt(str, shift = 7) {
+  return str.replace(/[A-Za-z]/g, c => 
+    String.fromCharCode((c <= 'Z' ? 90 : 122) >= (c = c.charCodeAt(0) + shift) ? c : c - 26)
+  );
+}
